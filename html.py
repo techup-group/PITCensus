db7283ec3a553b1c0bde426b621214f859925542
TehPwns <omgtehpwns@yahoo.com>
2016-06-04 17:26:44 -0400

    Added new question structure and HTML section

--- html.py
+++ html.py
@@ -4,13 +4,29 @@
 #The "general information" inputs on the index page
 # see index.html to see how these are rendered
 #
-# This is to the replaced with database inputs
+# Types of questions.
+# - Checkboxes (checkbox)
+# - Radio buttons (radio)
+# - Text entry (text)
+# -
 #
 index_general_inputs = {
 	"inputs": [
 		{"label": "Test Item 1", "id": "t1"},
 		{"label": "Test Item 2", "id": "t2"},
 		{"label": "Test Item 3", "id": "t3"},
+	],
+	"questions": [
+		{
+			"label": "Please answer this queston?",
+			"type": "radio",
+			"other_id": "14",
+		 	"inputs": [
+		 		{"label":"Answer1", "id":"11"},
+		 		{"label":"Answer2", "id":"12"},
+		 		{"label":"Other", "id":"13"}
+		 	]
+		}
 	]
 }
 
