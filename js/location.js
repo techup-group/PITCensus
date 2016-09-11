d88e6915a1dbb980729f4d67def9b9b38bfb3c47
TehPwns <waugh2@mail.usf.edu>
2016-09-11 14:37:21 -0400

    Map working again on Chrome - On Firefox, location data seems to not be acquired   - Otherwise, it would load

--- js/location.js
+++ js/location.js
@@ -1,60 +1,64 @@
- function embedGoogleMap(divId) 
- {
-     var location = getLocation();
+"use strict";
 
-     // Google-ready coordinates
-     var coords = new google.maps.LatLng(location.lat, location.long);
-
-     // set options for map (max zoom = 20)
-     var mapOptions = {
-         zoom: 16,
-         center: coords,
-         mapTypeId: google.maps.MapTypeId.ROADMAP
-     }
-         
-     // create map with marker at coordinates
-     var map = new google.maps.Map(document.getElementById(divId), mapOptions);
-     var marker = new google.maps.Marker({
-         map: map,
-         position: coords
-     });
- }
- 
-function getTimeString()
+function getTimeString() 
 {
-	var current_date = new Date();
-	return current_date.getHours() + ":" + current_date.getMinutes();
-}
- 
-function getDateString()
-{
-	var current_date = new Date();
-	return current_date.getMonth() + 1 + "/" + current_date.getDate() + "/" + current_date.getFullYear();
+    var current_date = new Date();
+    return current_date.getHours() + ":" + current_date.getMinutes();
 }
 
-function getLocation()
+function getDateString() 
 {
-     x = navigator.geolocation;
-     x.getCurrentPosition(success, failure);
-     var retlat = 0;
-     var retlong = 0;
-
-     function success(position) {
-         retlat = position.coords.latitude;
-         retlong = position.coords.longitude;
-     }
-
-     function failure() {
-         console.log("Failed to get position; user cancled?")
-     }
-     
- 	return { lat:retlat, long:retlong }    
+    var current_date = new Date();
+    return current_date.getMonth() + 1 + "/" + current_date.getDate() + "/" + current_date.getFullYear();
 }
 
-function getLocationString()
+function getLocation(callback) 
 {
-	var location = getLocation();
-	return location.lat + "|" + location.long;
+    function success(position) {
+        callback && callback(null, position.coords);
+    }
+
+    function failure() {
+        callback && callback("Failed to get position; user cancled?", null);
+    }
+
+    navigator.geolocation.getCurrentPosition(success, failure);
 }
 
+function embedGoogleMap(div) 
+{
+    getLocation(function(error, location) {
+        if (error) {
+            console.log("Failed to embed map: " + error);
+            return;
+        }
 
+        // Google-ready coordinates
+        var coords = new google.maps.LatLng(location.latitude, location.longitude);
+
+        // set options for map (max zoom = 20)
+        var mapOptions = {
+            zoom: 16,
+            center: coords,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        }
+
+        // create map with marker at coordinates
+        var map = new google.maps.Map(div, mapOptions);
+        var marker = new google.maps.Marker({
+            map: map,
+            position: coords
+        });
+    });
+}
+
+function getLocationString(callback) 
+{
+    getLocation(function(error, location) {
+        if (error) {
+            callback && callback(error, null);
+            return;
+        }
+        callback && callback(null, location.latitude + "|" + location.longitude);
+    });
+}
